08edfd406e7793f554217dfdf3e6c85cc0718d61
Navdeep_Mishra <navdeepmishra82734@gmail.com>
2021-01-14 17:03:16 +0530

    v1.0 launch

--- dnf_script.py
+++ dnf_script.py
@@ -17,24 +17,19 @@ class Main:
                 if  "dnf_script.py" in files and "1_directory_structure" in dirs: #just ignoring our original script and folder 
                     files.remove("dnf_script.py")
                     dirs.remove("1_directory_structure")
-                path = path.partition(self.search_path)[-1].lstrip('\\')
+                path = path.partition(self.search_path)[-1].lstrip('\\') #will break the path. our root path will be removed and rest will be processed
+                
                 if path:
                     os.mkdir(os.path.join(self.saving_path,path))
                 if(files):
-                    # files = [fn + '\n' for fn in files]
                     with open(os.path.join(self.saving_path,path,"1_directory_filelist.txt"),'w') as f:
                         for file in files:
-                            fi = str(file.encode("utf-8"))+"\n"
-                            f.write(fi.strip('b'))
+                            fi = str(file.encode("utf-8"))+"\n" #will encode the names in utf-8
+                            f.write(fi.strip('b')) #will remove the 'b' character from the names
                                    
             except FileExistsError:
                pass
             
-            
-          
-            
-
-
 if __name__ == '__main__':
     obj = Main(os.getcwd())
     obj.dir_blueprint_creater()
